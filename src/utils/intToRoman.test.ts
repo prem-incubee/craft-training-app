cd7ca36a6cf72702cb7f889eb98be6e1c2af3e54
Prajapati Jay <jay@incubyte.co>
2025-07-24 12:05:45 +0530

    mob next [ci-skip] [ci skip] [skip ci]
    lastFile:src/utils/intToRoman.test.ts

--- src/utils/intToRoman.test.ts
+++ src/utils/intToRoman.test.ts
@@ -1,7 +1,13 @@
 import { intToRoman } from "./intToRoman";
 
 describe("intToRoman", () => {
+
   it("returns empty string for 0", () => {
     expect(intToRoman(0)).toBe("");
   });
+
+  it('return I when given 1',()=>{
+    expect(intToRoman(1)).toBe("I");
+  })
+
 });
